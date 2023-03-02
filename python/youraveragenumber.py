35cd4a1be7cb411e016b1fdd1e442592649bdd10
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2023-03-02 14:38:18 +0300

    update xd

--- python/youraveragenumber.py
+++ python/youraveragenumber.py
@@ -1,12 +1,22 @@
 #author:mcagabe19
 
 import time
+#import traceback
+import logging
 
-number1 = int(input("Number 1 : "))
-number2 = int(input("Number 2 : "))
-number3 = int(input("Number 3 : "))
-number4 = int(input("Number 4 : "))
-total = number1 + number2 + number3 + number4 / 4
-print('Your Number Is : ', total)
-print('This Code Will Close After 3 Seconds...')
-time.sleep(3)
+try:
+    number1 = int(input("Number 1 : "))
+    number2 = int(input("Number 2 : "))
+    number3 = int(input("Number 3 : "))
+    number4 = int(input("Number 4 : "))
+    total = number1 + number2 + number3 + number4 / 4
+    print('Your Number Is : ', total)
+    print('This Code Will Close After 3 Seconds...')
+    time.sleep(3)
+
+except:
+    logging.exception("An exception was thrown!")
+    # print('Error!!!')
+    # traceback.print_exc()
+    print('This Code Will Close After 3 Seconds...')
+    time.sleep(3)
