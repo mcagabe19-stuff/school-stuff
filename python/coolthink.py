65dcfbe44e110472d06300a372ab25fe913fd370
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2023-03-02 13:48:05 +0300

    yay

--- python/coolthink.py
+++ python/coolthink.py
@@ -1,5 +1,7 @@
 #author:mcagabe19
 number1 = int(input("Number 1 : "))
 number2 = int(input("Number 2 : "))
-total = number1 + number2 / 2
+number3 = int(input("Number 3 : "))
+number4 = int(input("Number 4 : "))
+total = number1 + number2 + number3 + number4 / 4
 print('Your Number Is : ', total)
